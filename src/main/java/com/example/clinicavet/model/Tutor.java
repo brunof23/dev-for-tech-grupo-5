a8589fa0c27b3187589907fef6e01762d1975e87
jpaulo9 <jpaulosilva598@gmail.com>
2022-09-13 01:01:13 -0300

    recover

--- src/main/java/com/example/clinicavet/model/Tutor.java
+++ src/main/java/com/example/clinicavet/model/Tutor.java
@@ -4,7 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+<<<<<<< HEAD
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+=======
 import javax.persistence.*;
+>>>>>>> 88dd87e348127999f2325fbeed26326a9f9facc0
 
 @Data
 @AllArgsConstructor
@@ -15,6 +22,15 @@ public class Tutor {
     private static final long serialVersionUID = 1L;
 
     @Id
+<<<<<<< HEAD
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String contato;
+    //pet<chave estrangeira>
+=======
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
@@ -35,4 +51,5 @@ public class Tutor {
 
     //@JoinColumn(name = "addressId", referencedColumnName = "id")
     //private Address address;
+>>>>>>> 88dd87e348127999f2325fbeed26326a9f9facc0
 }
